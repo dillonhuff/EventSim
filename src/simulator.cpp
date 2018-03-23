3b6156f92051ddc96b0d7adb0b54291f49b13b38
Dillon Huff <dillonhuff@gmail.com>
2018-03-22 18:14:22 -0700

    Getting x values as returns now

--- src/simulator.cpp
+++ src/simulator.cpp
@@ -1,1 +1,28 @@
 #include "simulator.h"
+
+using namespace CoreIR;
+using namespace std;
+
+namespace EventSim {
+
+  BitVector extractBitVector(const WireValue& value) {
+    assert((value.getType() == WIRE_VALUE_ARRAY) ||
+           (value.getType() == WIRE_VALUE_BIT));
+
+    if (value.getType() == WIRE_VALUE_BIT) {
+      const BitValue& bitVal = static_cast<const BitValue&>(value);
+      BitVector bv(1, 0);
+      bv.set(0, bitVal.value());
+      return bv;
+    }
+
+    cout << "Value type = " << value.getType() << endl;
+    assert(value.getType() == WIRE_VALUE_ARRAY);
+
+    auto& val = static_cast<const ArrayValue&>(value);
+    BitVector bv(val.length(), 0);
+
+    return bv;
+  }
+
+}
--- src/simulator.h
+++ src/simulator.h
@@ -8,7 +8,7 @@ namespace EventSim {
 
   enum WireValueType {
     WIRE_VALUE_RECORD,
-    WIRE_VALUE_BIT_VECTOR,
+    WIRE_VALUE_ARRAY,
     WIRE_VALUE_BIT,
     WIRE_VALUE_NAMED
   };
@@ -21,24 +21,54 @@ namespace EventSim {
   };
 
   class RecordValue : public WireValue {
+    std::vector<std::pair<std::string, WireValue*> > fields;
+
   public:
+
+    RecordValue(const std::vector<std::pair<std::string, WireValue*> >& fields_) :
+      fields(fields_) {}
+
     virtual WireValueType getType() const { return WIRE_VALUE_RECORD; }
+
+    WireValue* getFieldValue(const std::string& fieldName) const {
+      for (auto& field : fields) {
+        if (field.first == fieldName) {
+          return field.second;
+        }
+      }
+
+      std::cout << "ERROR: Record does not contain field " << fieldName << std::endl;
+      assert(false);
+    }
   };
 
   class ArrayValue : public WireValue {
+    std::vector<WireValue*> elems;
+
   public:
-    virtual WireValueType getType() const = 0;
+    ArrayValue(const std::vector<WireValue*>& elems_) : elems(elems_) {}
+
+    virtual WireValueType getType() const { return WIRE_VALUE_ARRAY; }
+
+    int length() const { return elems.size(); }
   };
 
   class BitValue : public WireValue {
+    bsim::quad_value bitVal;
+
   public:
-    virtual WireValueType getType() const = 0;
+    BitValue(const bsim::quad_value& bitVal_) : bitVal(bitVal_) {}
+    virtual WireValueType getType() const { return WIRE_VALUE_BIT; }
+
+    bsim::quad_value value() const { return bitVal; }
   };
 
   class NamedValue : public WireValue {
   public:
     virtual WireValueType getType() const = 0;
   };
+
+  BitVector extractBitVector(const WireValue& value);
   
   class EventSimulator {
     CoreIR::Module* mod;
@@ -66,13 +96,35 @@ namespace EventSim {
     WireValue* defaultWireValue(CoreIR::Wireable* const w) {
       WireValue* val = nullptr;
       if (w->getType()->getKind() == CoreIR::Type::TK_Record) {
-        val = new RecordValue();
-        wireValues.insert(val);
+
+        std::vector<std::pair<std::string, WireValue*> > fields;
+        CoreIR::RecordType* tp = CoreIR::cast<CoreIR::RecordType>(w->getType());
+        for (auto field : tp->getFields()) {
+          fields.push_back({field, defaultWireValue(w->sel(field))});
+        }
+        val = new RecordValue(fields);
+        
+      } else if (CoreIR::isa<CoreIR::ArrayType>(w->getType())) {
+
+        CoreIR::ArrayType* arrTp = CoreIR::cast<CoreIR::ArrayType>(w->getType());
+        //CoreIR::ArrayType* elemTp = arrTyp->getElemType();
+
+        std::vector<WireValue*> arrValues;
+        for (int i = 0; i < (int) arrTp->getLen(); i++) {
+          arrValues.push_back(defaultWireValue(w->sel(i)));
+        }
+        val = new ArrayValue(arrValues);
+
+      } else if (isBitType(*(w->getType()))) {
+        val = new BitValue(bsim::quad_value(QBV_UNKNOWN_VALUE));
       } else {
+        std::cout << "ERROR: Unsupported wireable " << w->toString() << std::endl;
         assert(false);
       }
 
       assert(val != nullptr);
+
+      wireValues.insert(val);
 
       return val;
     }
@@ -84,7 +136,6 @@ namespace EventSim {
       assert(CoreIR::isa<CoreIR::Select>(s));
 
       CoreIR::Select* sel = CoreIR::cast<CoreIR::Select>(s);
-
       CoreIR::Wireable* top = sel->getTopParent();
 
       std::cout << "Top = " << top->toString() << std::endl;
@@ -92,10 +143,49 @@ namespace EventSim {
 
       WireValue* v = values.at(top);
       assert(v != nullptr);
+
+      // Update bits stored in v
+    }
+
+    WireValue* selectField(const std::string& selStr,
+                           const WireValue* const w) const {
+      WireValueType tp = w->getType();
+
+      assert(tp != WIRE_VALUE_BIT);
+      assert(tp != WIRE_VALUE_NAMED);
+
+      if (tp == WIRE_VALUE_RECORD) {
+        const RecordValue* const r = static_cast<const RecordValue* const>(w);
+        return r->getFieldValue(selStr);
+      }
+
+      assert(false);
+    }
+
+    WireValue* getWireValue(CoreIR::Wireable* const w) const {
+      if (!CoreIR::isa<CoreIR::Select>(w)) {
+        assert(contains_key(w, values));
+
+        WireValue* v = values.at(w);
+
+        return v;
+      }
+
+      assert(CoreIR::isa<CoreIR::Select>(w));
+
+      CoreIR::Select* const sel = CoreIR::cast<CoreIR::Select>(w);
+      const std::string& selStr = sel->getSelStr();
+
+      WireValue* parent = getWireValue(sel->getParent());
+      
+      return selectField(selStr, parent);
     }
 
     BitVector getBitVec(const std::string& name) {
-      return BitVector(1, 1);
+      assert(mod->getDef()->canSel(name));
+
+      WireValue* wv = getWireValue(mod->getDef()->sel(name));
+      return extractBitVector(*wv);
     }
 
     ~EventSimulator() {
